794194573ecf9f6ebb75604fe423279e8e7f0073
Hajin Suh <hajin.c.suh@vanderbilt.edu>
2024-12-04 16:14:25 -0600

    test

--- Spark/mapreduce_spark.py
+++ Spark/mapreduce_spark.py
@@ -4,12 +4,13 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, count
 
 db_ip = "database"
+db_name = "images_database"
 COUCHDB_URL = f"http://{db_ip}:5984"
 USERNAME = "team"
 PASSWORD = "cloudcomputing"
 LOCAL_JSON_FILE = "/home/cc/team17/PA4/tmp/couchdb_data.json"
 
-response = requests.get(f"{COUCHDB_URL}/{"images_database"}",
+response = requests.get(f"{COUCHDB_URL}/{db_name}",
                             auth=(USERNAME, PASSWORD))
 if response.status_code == 200:
     data = response.json()
