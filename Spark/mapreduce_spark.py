0897c3a2518b45c50886cab0f974ec7bcc26518d
Hajin Suh <hajin.c.suh@vanderbilt.edu>
2024-12-05 15:35:27 -0600

    test

--- Spark/mapreduce_spark.py
+++ Spark/mapreduce_spark.py
@@ -20,24 +20,24 @@ def get_all_documents(COUCHDB_URL, DB_NAME, USERNAME, PASSWORD):
         print(f"Error fetching documents: {response.json()}")
         return []
 
-spark = SparkSession.builder \
-    .appName("Incorrect Predictions Count") \
-    .getOrCreate()
+# spark = SparkSession.builder \
+#     .appName("Incorrect Predictions Count") \
+#     .getOrCreate()
 
 documents = get_all_documents(COUCHDB_URL, DB_NAME, USERNAME, PASSWORD)
 
-data = []
-for doc in documents:
-    if 'doc' in doc:
-        record = doc['doc']
-        # Ensure both ground_truth and prediction are present
-        if 'ground_truth' in record and 'prediction' in record:
-            data.append((record['ground_truth'], record['prediction']))
+# data = []
+# for doc in documents:
+#     if 'doc' in doc:
+#         record = doc['doc']
+#         # Ensure both ground_truth and prediction are present
+#         if 'ground_truth' in record and 'prediction' in record:
+#             data.append((record['ground_truth'], record['prediction']))
 
-df = spark.createDataFrame(data, ["ground_truth", "prediction"])
+# df = spark.createDataFrame(data, ["ground_truth", "prediction"])
 
-incorrect_predictions_count = df.filter(col("ground_truth") != col("prediction")).count()
+# incorrect_predictions_count = df.filter(col("ground_truth") != col("prediction")).count()
 
-print(f"Total incorrect predictions: {incorrect_predictions_count}")
+# print(f"Total incorrect predictions: {incorrect_predictions_count}")
 
-spark.stop()
+# spark.stop()
