0236578df04d5d5f8161909a6a7fa70eba8d940f
Hajin Suh <hajin.c.suh@vanderbilt.edu>
2024-12-06 10:52:48 -0600

    test

--- Spark/mapreduce_spark.py
+++ Spark/mapreduce_spark.py
@@ -9,7 +9,7 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # local_json_path = "file:///home/cc/team17/PA4/Spark/target/incorrect_count.json"
-local_json_path = "/app/incorrect_count.json"
+local_json_path = "file:///app/incorrect_count.json"
 # spark.sparkContext.addFile(local_json_path)
 
 # json_file_path = SparkFiles.get("incorrect_count.json")
