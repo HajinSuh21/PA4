6086a70141c46688b213aa44823217bc7eeba874
Hajin Suh <hajin.c.suh@vanderbilt.edu>
2024-12-06 10:10:31 -0600

    test

--- Spark/mapreduce_spark.py
+++ Spark/mapreduce_spark.py
@@ -13,7 +13,7 @@ local_json_path = "file:///home/cc/team17/PA4/Spark/target/incorrect_count.json"
 
 # json_file_path = SparkFiles.get("incorrect_count.json")
 
-df = spark.read.json(json_file_path)
+df = spark.read.json(local_json_path)
 
 df_transformed = df.select(explode(col("*")).alias("data")) \
     .selectExpr("data.Latency as Latency", "data.IsCorrect as IsCorrect")
