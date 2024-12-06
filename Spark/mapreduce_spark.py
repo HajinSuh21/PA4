be0f8015c849577cb7314d464b99866a2b8eb6be
Hajin Suh <hajin.c.suh@vanderbilt.edu>
2024-12-06 00:22:13 -0600

    test

--- Spark/mapreduce_spark.py
+++ Spark/mapreduce_spark.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, explode
+from pyspark import SparkFiles
 import requests
 import json
 
@@ -8,7 +9,11 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 incorrect_count_json = "file:///home/cc/team17/PA4/Spark/target/incorrect_count.json"
-df = spark.read.json(incorrect_count_json)
+spark.sparkContext.addFile(incorrect_count_json)
+
+json_file_path = SparkFiles.get("incorrect_count.json")
+
+df = spark.read.json(json_file_path)
 
 df_transformed = df.select(explode(col("*")).alias("data")) \
     .selectExpr("data.Latency as Latency", "data.IsCorrect as IsCorrect")
